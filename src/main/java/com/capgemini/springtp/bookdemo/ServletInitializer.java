b98c16ac0dee23167c2180a2e68923813a15e487
Krzysztof Sobkowiak <krzysztof.sobkowiak@capgemini.com>
2015-02-16 21:05:40 +0100

    Initial version

---
+++ src/main/java/com/capgemini/springtp/bookdemo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.capgemini.springtp.bookdemo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BookDemoApplication.class);
+	}
+
+}
